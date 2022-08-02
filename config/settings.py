dbee6d3cf51315f09d7033ace0f38d3d80b77486
jongheon lee <tlqkfwjrekdgl@gmail.com>
2022-08-02 20:47:09 +0900

    team

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'team.apps.TeamConfig',
     'user.apps.UserConfig',
     'pybo.apps.PyboConfig',
     'django.contrib.admin',
--- team/models.py
+++ team/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from user.models import User
 
-# class team(models.Model):
-#     teamname = models.CharField(max_length=30)
-#     description = models.CharField(max_length=30)
-#     manager = models.CharField(max_length=30)
-#     member = models.CharField(max_length=30)
-#     task = models.CharField(max_length=30)
+class manager(models.Model):
+    name = models.ForeignKey(User,on_delete=models.CASCADE)
+
+class member(models.Model):
+    member = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+class team(models.Model):
+    teamname = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    manager = models.ManyToManyField(manager)
+    member = models.ManyToManyField(member)
+    task = models.CharField(max_length=30)
