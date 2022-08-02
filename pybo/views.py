117c044774dbffd1dc6225c3b5255d8553d67ef5
Gronrue <gronrue>
2022-08-02 18:28:15 +0900

    qq

--- pybo/views.py
+++ pybo/views.py
@@ -6,15 +6,18 @@ from .models import Question
 from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm
 
+
 def index(request):
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
 
+
 def detail(request, question_id):
     question = Question.objects.get(id=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
 
 def question_create(request):
     if request.method == 'POST':
