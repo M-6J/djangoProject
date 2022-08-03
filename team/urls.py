66461d6fecb21f5cbfaddef21ac87be097d415af
jongheon lee <tlqkfwjrekdgl@gmail.com>
2022-08-03 22:25:37 +0900

    v2

--- team/urls.py
+++ team/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'team'
 
 urlpatterns = [
-    path('', views.detail, name='detail'),
+    path('', views.index, name='index'),
+    path('<int:team_id>/detail', views.detail, name='detail'),
+    path('<int:team_id>/delete', views.delete, name='delete'),
+    path('<int:team_id>/modify', views.modify, name='modify'),
 ]
--- team/views.py
+++ team/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 from .models import Team
 
-def detail(request):
+def index(request):
 
     team_list = Team.objects.all()
     context = {'team_list': team_list}
-    return render(request, 'team/team_detail.html', context)
+    return render(request, 'team/team_list.html', context)
+
+def detail(request,team_id):
+    team = Team.objects.get(id=team_id)
+    context = {'team': team}
+    return render(request, 'team/team_detail.html',context)
+
+def delete(request,team_id):
+    team = Team.objects.get(id=team_id)
+    team.delete()
+    return redirect('/team')
+
+def modify(request,team_id):
+    team = Team.objects.get(id=team_id)
+    context = {'team': team}
+    return render(request,'team/team_modify.html',context)
