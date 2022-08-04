26a576c53f09789f923d909e5cb6ea3341e24161
M-6J <volatus_mj@naver.com>
2022-08-04 15:53:33 +0900

    init

--- pybo/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Question(models.Model):
-    subject = models.CharField(max_length=200)
-    content = models.TextField()
-    create_date = models.DateTimeField()
-    # project manager , group name
-    group_name = models.CharField(max_length=200,default='task')
