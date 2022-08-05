dd67c315731038cca3dc4dad4ff439ae268c2917
Gronrue <gronrue>
2022-08-05 15:51:04 +0900

    input resolve

--- projectApp/views.py
+++ projectApp/views.py
@@ -4,23 +4,19 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def manage(request):
-
     pass
 
 
 @csrf_exempt
 def create(request):
-
     pass
 
 
 @csrf_exempt
 def detail(request):
-
     pass
 
 
 @csrf_exempt
 def update(request):
-
     pass
